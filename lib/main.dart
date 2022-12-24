b1be87a1a92ad90064fc4b0d0b8334c8597da0e3
Mayar Mahmoud <mahmoudmayar251@gmail.com>
2022-12-24 23:51:32 +0200

    finish firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/layout/home_page.dart';
@@ -5,7 +6,9 @@ import 'package:todo_app/myProvider/my_provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:todo_app/shared/styles/theme.dart';
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ChangeNotifierProvider(
       create: (context)=>MyProvider(),
       child: MyApp()));
